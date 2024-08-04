183c79846b726a074770e4172fca8172169f35f2
Neidy22 <3236875710511@ingenieria.usac.edu.gt>
2024-08-04 17:11:56 -0600

    Instructions

---
+++ actividad2/cronjob.md
@@ -0,0 +1,11 @@
+# Agregar el Cronjob al crontab
+1. crontab -e
+2. */5 * * * * cd ruta/so1_actividades_201801671/actividad2 && ./script.sh
+
+3. Guardar los cambios CTRL + X, Y, Enter
+
+# Eliminar el Cronjob
+1. crontab -e
+2. eliminar la linea que contiene el cronjob anteriormente creado
+3. Guardar los cambios
+
--- actividad2/script.sh
+++ actividad2/script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
+
 #Obtner la fecha de ejecucion
 EXECUTION_DATE=$(date +"%F-%H-%M-%S")
 
-# leer nombre de usuario
-GITHUB_USER="Neidy22"
+# echo $GITHUB_USER
 
-# obtener info de la api de github
+# leer nombre de usuario de las variables de entornor y obtener info de la api de github
 USER_INFO=$(curl -s "https://api.github.com/users/$GITHUB_USER")
 
-github_user=$(echo $USER_INFO | jq -r '.name')
+git_user=$(echo $USER_INFO | jq -r '.name')
 id=$(echo $USER_INFO | jq -r '.id')
 created_at=$(echo $USER_INFO | jq -r '.created_at')
 
@@ -17,10 +17,9 @@ created_at=$(echo $USER_INFO | jq -r '.created_at')
 # Crear el archivo log
 LOG_FILE_PATH="tmp/$EXECUTION_DATE/saludos.log"
 mkdir -p tmp/$EXECUTION_DATE/
-#touch $LOG_FILE_PATH
 
 #Escribir el mensaje en el archivo saludos.log
-msg="Hola $github_user. User ID: $id. Cuenta fue creada el: $created_at"
+msg="Hola $git_user. User ID: $id. Cuenta fue creada el: $created_at"
 
 echo $msg >> $LOG_FILE_PATH
 
